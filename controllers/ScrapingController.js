2089f934a8ad545ec82b1584377b8e56c6ab34a6
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2023-02-23 13:29:14 -0300

    performance improvement

--- controllers/ScrapingController.js
+++ controllers/ScrapingController.js
@@ -7,16 +7,24 @@ class ScrapingController {
     const linkSite = process.env.LYRICS_URL
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+    await page.setRequestInterception(true);
+
+    // DISABLE UNNECESSARY LOADS
+    page.on('request', (req) => {
+      if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+        req.abort();
+      }
+      else {
+        req.continue();
+      }
+    });
+
     await page.goto(`${linkSite}/?q=${musicName}`);
     await page.waitForSelector('.gs-title');
     await page.click('.gs-title > a');
     await page.waitForSelector('.cnt-head');
     const url = page.url();
-    if (url.includes("traducao.html")) {
-      const baseUrl = url.replace("/traducao.html", "");
-      await page.goto(baseUrl);
-    }
-    const lyric = await page.$('.cnt-letra');
+    const lyric = await page.$(url.includes("traducao.html") ? '.cnt-trad_l' : '.cnt-letra');
     let value = await page.evaluate(el => el.innerText, lyric)
     res.status(200).json({ lyric: value });
     await browser.close();
