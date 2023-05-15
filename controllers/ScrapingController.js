c9bf49aab7674c531d6d3ca6dbb795f6864fc2c7
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2023-05-15 10:16:29 -0300

    att bugs

--- controllers/ScrapingController.js
+++ controllers/ScrapingController.js
@@ -3,30 +3,35 @@ require('dotenv').config();
 
 class ScrapingController {
   static async getLyric(req, res) {
-    const { musicName } = req.params;
-    const linkSite = process.env.LYRICS_URL
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
+    try {
+      const { musicName } = req.params;
+      const linkSite = process.env.LYRICS_URL
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.setRequestInterception(true);
 
-    // DISABLE UNNECESSARY LOADS
-    page.on('request', (req) => {
-      if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
-        req.abort();
-      }
-      else {
-        req.continue();
-      }
-    });
+      // DISABLE UNNECESSARY LOADS
+      page.on('request', (req) => {
+        if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+          req.abort();
+        }
+        else {
+          req.continue();
+        }
+      });
 
-    await page.goto(`${linkSite}/?q=${musicName}`);
-    await page.waitForSelector('.gs-title');
-    await page.click('.gs-title > a');
-    const url = page.url();
-    const lyric = await page.$(url.includes("traducao.html") ? '.cnt-trad_l' : '.cnt-letra');
-    let value = await page.evaluate(el => el.innerText, lyric)
-    res.status(200).json({ lyric: value });
-    await browser.close();
+      page.goto(`${linkSite}/?q=${musicName}`);
+      await page.waitForSelector('.gs-title');
+      await page.click('.gs-title > a');
+      const url = page.url();
+      const lyric = await page.$(url.includes("traducao.html") ? '.cnt-trad_l' : '.cnt-letra');
+      let value = await page.evaluate(el => el.innerText, lyric);
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      await browser.close();
+      return res.status(200).json({ lyric: value });
+    } catch (error) {
+      return res.status(400).json({ error: "Error, try again." });
+    }
   }
 }
 
